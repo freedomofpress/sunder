8713a6612248d9cc351fea2c8932a1bfdd7d7ae4
Gabe Isman <gabe.isman@gmail.com>
2016-04-04 17:26:31 -0700

    Very basic home page.

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,16 +8,26 @@ export default class Home extends Component {
   render() {
     return (
       <div className="container home">
-        <Button type="xlarge"
-          icon="cubes"
-          onClick={() => this.context.router.push('split')}>
-          Split a secret
-        </Button>
-        <Button type="xlarge"
-          icon="cube"
-          onClick={() => this.context.router.push('recover')}>
-          Recover a secret
-        </Button>
+        <h1 className="home-title align-center">
+          Rusty Secrets
+        </h1>
+        <p className="home-explanation">
+          {"Rusty Secrets is an implementation of Shamir's Secret Sharing scheme. " +
+            "Under this scheme a secret is split into some number of pieces, called 'shares.'" +
+            'A configurable quorum of these shares is required to recover the original secret. '}
+        </p>
+        <div className="home-actions align-center">
+          <Button type="xlarge"
+            icon="cubes"
+            onClick={() => this.context.router.push('split')}>
+            Split a secret
+          </Button>
+          <Button type="xlarge"
+            icon="cube"
+            onClick={() => this.context.router.push('recover')}>
+            Recover a secret
+          </Button>
+        </div>
       </div>
     );
   }
