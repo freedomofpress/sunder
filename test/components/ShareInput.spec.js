e545e66c6b27a3bfe958ff7893ae378a0b95ced2
Gabe Isman <gabe.isman@gmail.com>
2016-04-25 13:27:34 -0700

    Test for ShareInput

---
+++ test/components/ShareInput.spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import { spy, match } from 'sinon';
+import { ShareInput } from 'app/components/ShareInput';
+import Button from 'app/components/Button';
+
+const props = {
+  dispatch: spy(),
+  quorum: 3,
+  numEnteredShares: 2
+};
+
+describe('<ShareInput />', () => {
+  it('should call addShare when button is pressed', () => {
+    const shareInput = mount(<ShareInput {...props} />);
+    const input = shareInput.find('[name="secret-share"]');
+    const button = shareInput.find(Button);
+    const testShare = 'test string';
+    input.get(0).value = testShare;
+    button.simulate('click');
+    // This is a little awkward to test thoroughly, but this is pretty good.
+    expect(props.dispatch.calledWithMatch(match.func)).to.be.true();
+  });
+});
