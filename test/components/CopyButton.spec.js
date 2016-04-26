f42928f06aa3ee3602f97ce230457c0d532365f3
Gabe Isman <gabe.isman@gmail.com>
2016-04-26 12:34:52 -0700

    CopyButton test.

---
+++ test/components/CopyButton.spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import CopyButton from 'app/components/CopyButton';
+
+const props = {
+  targetText: 'xyz'
+};
+
+describe('<CopyButton />', () => {
+  it('should render without error', () => {
+    expect(shallow(<CopyButton {...props} />)).to.exist();
+  });
+
+  // The rest of the functionality here somes hard to test/not worth testing.
+});
