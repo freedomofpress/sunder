a0d225dbb116f66586ee29dc3577524db5f0e550
Gabe Isman <gabe.isman@gmail.com>
2016-04-25 17:14:59 -0700

    NumberField tests.

---
+++ test/components/NumberField.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import { spy } from 'sinon';
+import NumberField from 'app/components/NumberField';
+
+describe('NumberField', () => {
+  it('should render an error message', () => {
+    const props = {
+      field: {
+        error: 'error',
+        touched: true,
+        invalid: true
+      }
+    };
+    expect(shallow(<NumberField {...props} />).find('.error-label')).to.have.length(1);
+  });
+
+  it('should call onChanged with the parsed value', () => {
+    const props = { field: { onChange: spy() } };
+    const field = shallow(<NumberField {...props} />);
+    const input = field.find('input');
+    input.simulate('change', { target: { value: '10' } });
+    expect(props.field.onChange.calledWith(10)).to.be.true();
+  });
+});
