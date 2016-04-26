8b481662d2935718f1641e9dff806f9dc15475f7
Gabe Isman <gabe.isman@gmail.com>
2016-04-26 12:51:17 -0700

    Layout test.

---
+++ test/components/Layout.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import Layout from 'app/components/Layout';
+
+const props = { header: 'xyz', children: 'some txt' };
+
+describe('<Layout />', () => {
+  it('should render without error', () => {
+    const layout = shallow(<Layout {...props} />);
+    expect(layout.text()).to.contain(props.children);
+    expect(layout.text()).to.contain(props.header);
+  });
+});
