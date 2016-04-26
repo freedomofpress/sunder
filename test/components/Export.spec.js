e4c92df1d14686b65f053cbc6c86e46323298260
Gabe Isman <gabe.isman@gmail.com>
2016-04-26 11:54:43 -0700

    Tests for Export

---
+++ test/components/Export.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import Export from 'app/components/Export';
+import Modal from 'app/components/Modal';
+
+const props = { secret: 'xyz' };
+
+describe('<Export />', () => {
+  it('should render without error', () => {
+    expect(shallow(<Export {...props} />)).to.exist();
+  });
+
+  it('should show the view model when view clicked', () => {
+    const wrapper = mount(<Export {...props} />);
+    expect(wrapper.find(Modal)).to.have.length(0);
+    wrapper.find('#view-secret-button').simulate('click');
+    expect(wrapper.find(Modal)).to.have.length(1);
+  });
+});
