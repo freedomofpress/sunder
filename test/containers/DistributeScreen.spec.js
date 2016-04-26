da8ece597517a677e20270552fb581d1447ff212
Gabe Isman <gabe.isman@gmail.com>
2016-04-26 13:18:00 -0700

    ExportScreen test.

--- test/containers/DistributeScreen.spec.js
+++ test/containers/DistributeScreen.spec.js
@@ -7,7 +7,7 @@ import { DistributeScreen } from 'app/containers/DistributeScreen';
 const props = { dispatch: spy(), shares: ['xyz'], quorum: 1 };
 
 describe('<DistributeScreen />', () => {
-  it('should reset app state when loaded', () => {
+  it('should render without error', () => {
     const distribute = mount(<DistributeScreen {...props} />);
     expect(distribute).to.exist();
   });
---
+++ test/containers/ExportScreen.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import { spy } from 'sinon';
+import { ExportScreen } from 'app/containers/ExportScreen';
+
+const props = { dispatch: spy(), secret: 'zyz' };
+
+describe('<ExportScreen />', () => {
+  it('should render without error', () => {
+    const screen = mount(<ExportScreen {...props} />);
+    expect(screen).to.exist();
+  });
+});
