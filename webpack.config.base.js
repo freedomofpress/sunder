54cffd799508ed2e21ee5e8c17479a39d85e48ca
Gabe Isman <gabe.isman@gmail.com>
2016-05-11 10:22:29 -0500

    Fix font-awesome.

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -9,6 +9,15 @@ module.exports = {
     }, {
       test: /\.json$/,
       loader: 'json-loader'
+    }, {
+      test: /\.css$/,
+      loaders: ['style-loader', 'css-loader']
+    }, {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+    }, {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'file-loader'
     }]
   },
   output: {
