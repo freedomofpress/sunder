c30365df0265dd4ffca34b2e1f386da351f0a7d8
Gabe Isman <gabe.isman@gmail.com>
2017-04-29 21:07:00 -0400

    Explicitly require prop-types, per deprecation.

--- src/components/Export.js
+++ src/components/Export.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Export.scss';
 import Panel from './Panel';
 import Button from './Button';
--- src/containers/DistributeScreen.js
+++ src/containers/DistributeScreen.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Distribute from '../components/Distribute';
 import HomeButton from '../components/HomeButton';
