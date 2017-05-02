88560513306dbb7b2fe6ec0cb1ba97945afc0bde
Gabe Isman <gabe.isman@gmail.com>
2017-05-01 23:18:51 -0400

    Fix icon test.

--- test/components/Icon.spec.js
+++ test/components/Icon.spec.js
@@ -3,11 +3,11 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import Icon from 'src/components/Icon';
 
-const props = { className: 'test' };
+const props = { type: 'recover' };
 
 describe('<Icon />', () => {
   it('should render without error', () => {
     const icon = shallow(<Icon {...props} />);
-    expect(icon.is(`.${props.className}`)).to.be.true();
+    expect(icon.is('svg')).to.be.true();
   });
 });
