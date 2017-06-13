3730acf2f44c7f12e21631fae2791893ff5dbbd6
Garrett Robinson <garrett.f.robinson@gmail.com>
2017-06-13 16:02:40 -0700

    Use Spectron for e2e tests
    This is a minimal commit that replaces chromedriver and
    selenium-webdriver with Spectron, and makes the changes necessary to get
    the e2e tests passing using Spectron.
    
    Most of the changes were necessitated by Spectron's use of WebdriverIO
    instead of the old WebDriver API.

--- test/e2e.js
+++ test/e2e.js
@@ -1,11 +1,9 @@
+const Application = require('spectron').Application;
 import path from 'path';
-import chromedriver from 'chromedriver';
-import webdriver from 'selenium-webdriver';
 import { expect } from 'chai';
 import electronPath from 'electron';
 
-chromedriver.start(); // on port 9515
-process.on('exit', chromedriver.stop);
+const appPath = path.join(__dirname, '..');
 
 const delay = time => new Promise(resolve => setTimeout(resolve, time));
 
@@ -16,90 +14,81 @@ describe('main window', function spec() {
   const numShares = 3;
   const secret = 'test secret';
 
-  before(async () => {
-    await delay(1000); // wait chromedriver start time
-    this.driver = new webdriver.Builder()
-      .usingServer('http://localhost:9515')
-      .withCapabilities({
-        chromeOptions: {
-          binary: electronPath,
-          args: [`app=${path.resolve()}`]
-        }
-      })
-      .forBrowser('electron')
-      .build();
+  let app;
+
+  before(() => {
+    app = new Application({
+      path: electronPath,
+      env: { SPECTRON: true },
+      args: [appPath],
+    });
+    return app.start();
   });
 
-  after(async () => {
-    await this.driver.quit();
+  after(() => {
+    return app.stop();
   });
 
   it('should open window', async () => {
-    const title = await this.driver.getTitle();
+    const title = await app.client.getTitle();
     expect(title).to.equal('Sunder');
   });
 
   it('should click on the split button', async () => {
-    await this.driver.findElement({ id: 'split-button' }).click();
+    await app.client.element('#split-button').click();
   });
 
   it('should enter some bogus data on the split screen', async () => {
-    await this.driver.findElement({ name: 'secret' }).sendKeys(secret);
-    await this.driver.findElement({ name: 'quorum' }).sendKeys(quorum);
-    await this.driver.findElement({ name: 'shares' }).sendKeys(numShares);
-    await this.driver.findElement({ id: 'create-shares' }).click();
+    await app.client.element('[name=secret]').setValue(secret);
+    await app.client.element('[name=quorum]').setValue(quorum);
+    await app.client.element('[name=shares]').setValue(numShares);
+    await app.client.element('#create-shares').click();
   });
 
   it('should find the right number of shares on distribute screen', async () => {
     await delay(2100); // wait for fake delay
-    const shareEls = await this.driver.findElements({ className: 'share-row' });
-    expect(shareEls).to.have.length(3);
+    const shareEls = await app.client.elements('.share-row');
+    expect(shareEls.value).to.have.length(3);
   });
 
   it('should find and store the share values', async () => {
     // Depends on implementation of the copy button, less than ideal.
-    const shareEls = await this.driver.findElements({ css: 'input[readonly]' });
-    return Promise.all(shareEls.map((el) => el.getAttribute('value'))).then((values) => {
-      // Store in global for later use
-      shares = values;
-    });
+    shares = await app.client.getAttribute('input[readonly]', 'value');
   });
 
   it('should go back to the home screen', async () => {
-    await this.driver.findElement({ className: 'btn-back' }).click();
+    await app.client.element('.btn-back').click();
   });
 
   it('should navigate to recover', async () => {
-    await this.driver.findElement({ id: 'recover-button' }).click();
+    await app.client.element('#recover-button').click();
   });
 
   it('should select text input', async () => {
-    await this.driver.findElement({ css: 'option[value="text"]' }).click();
+    await app.client.selectByValue('div.file-or-text-mode-select-container select', 'text')
   });
 
   it('should input the shares', async () => {
     for (let i = 0; i < quorum; i++) {
-      await this.driver.executeScript(
-        "arguments[0].value = arguments[1]; arguments[0].dispatchEvent(new Event('blur'));",
-        this.driver.findElement({ name: 'share' }),
+      await app.client.execute(
+        "var el = document.querySelector(arguments[0]); el.value = arguments[1]; el.dispatchEvent(new Event('blur'));",
+        '[name=share]',
         shares[i]);
-      await delay(1000);
-      await this.driver.findElement({ id: 'submit-share-button' }).click();
-      await delay(1000);
+      await app.client.element('#submit-share-button').click();
     }
   });
 
   it('should click the finish recovery button', async () => {
-    await this.driver.findElement({ id: 'finish-recovery' }).click();
+    await app.client.element('#finish-recovery').click();
     await delay(2100); // wait for fake delay
   });
 
   it('should click view secret button', async () => {
-    await this.driver.findElement({ id: 'view-secret-button' }).click();
+    await app.client.element('#view-secret-button').click();
   });
 
   it('should have the right secret revealed', async () => {
-    const foundSecret = await this.driver.findElement({ className: 'secret-view' })
+    const foundSecret = await app.client.element('.secret-view')
         .getAttribute('value');
     expect(foundSecret).to.be.eql(secret);
   });
