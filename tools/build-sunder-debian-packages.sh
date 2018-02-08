a996ce5461eb1359ea2d14acf1340be702f75e89
Conor Schaefer <conor@freedom.press>
2018-02-08 12:23:34 -0800

    Creates build script for container deb packages
    Not folding this logic into the Dockerfile build info, since it requires
    the application code to be mounted inside the container. The volume
    requirement mandates that the `npm` commands in the build script be run
    as a container "run" action, not as part of the container "build"
    action.

---
+++ tools/build-sunder-debian-packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+set -u
+
+
+SUNDER_CODE=/sunder
+
+if [[ ! -d "${SUNDER_CODE}" ]]; then
+    printf "${SUNDER_CODE} directory not found!\\n"
+    exit 1
+fi
+
+icns2png --extract --output "${SUNDER_CODE}/build/icons/" "${SUNDER_CODE}/build/icon.icns"
+npm install
+npm rebuild
+npm run dist
